3e319a64fb0564a098b1610271617f6e9e4456f8
Peterson Nascimento <petodonal@gmail.com>
2025-08-25 21:33:18 -0300

    PS

--- PowerShell/001.ps1
+++ PowerShell/001.ps1
@@ -1,3 +1,5 @@
 # 001. **Olá e nome.** Imprima 'Olá, mundo!' e pergunte o nome; mostre 'Bem-vindo, <nome>'.
 
 write-host "Ola mundo"
+$nome = read-host "Digite o seu nome"
+write-host "Bem vindo $nome"
