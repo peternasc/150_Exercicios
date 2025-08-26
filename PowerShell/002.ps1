2791a280dda40ae59ca026dc0507bb08fd57a3e1
peternasc <petodonald@gmail.com>
2025-08-26 08:21:02 -0300

    PowerShell 002

---
+++ PowerShell/002.ps1
@@ -0,0 +1,8 @@
+﻿# Soma simples. Leia dois inteiros e mostre a soma. (tags: io, aritmetica)
+
+[int]$num1 = Read-host "Digite o primeiro número"
+[int]$num2 = Read-Host "Digite o segundo número"
+$soma = $num1 + $num2
+
+write-host "A soma entre $num1 e $num2 é $soma"
+
